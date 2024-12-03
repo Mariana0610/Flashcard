a20faca1f02c3a2ac083202c529d7af115cbda9e
Mariana0610 <00001108084333sp@al.educacao.sp.gov.br>
2024-12-03 14:56:57 -0300

    projeto desenvolvido pelo alura

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Programação',
+    'O que é Phyton?',
+    'O Phyton é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Progrmação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em Inglês é HI(RAI)'
+    
+)
+
